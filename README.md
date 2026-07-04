8511e86053350b2868ffe9f98bbb1e83a52fd4cc
Aluis Marte <aluismarte@gmail.com>
2026-07-04 17:10:41 -0400

    init: initial project setup with Maven wrapper and config files

---
+++ README.md
@@ -0,0 +1,5 @@
+# JConf Dominicana 2026 Session
+
+[![Java](https://img.shields.io/badge/Java-25-red.svg)](https://www.oracle.com/java/)
+[![Spring Boot](https://img.shields.io/badge/Spring%20Boot-4.1.0-green.svg)](https://spring.io/projects/spring-boot)
+[![Maven](https://img.shields.io/badge/Maven-3.9.15-blue.svg)](https://maven.apache.org/)
---
+++ src/main/java/org/alsjava/sessions/configuration/ObjectMapperConfiguration.java
@@ -0,0 +1,34 @@
+package org.alsjava.sessions.configuration;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.jackson.autoconfigure.JsonMapperBuilderCustomizer;
+import org.springframework.boot.jackson.autoconfigure.XmlMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import tools.jackson.databind.DeserializationFeature;
+import tools.jackson.databind.cfg.DateTimeFeature;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class ObjectMapperConfiguration {
+
+    @Bean
+    public JsonMapperBuilderCustomizer jsonMapperBuilderCustomizer() {
+        return jsonMapperBuilder -> {
+            log.info("Applying JsonMapperBuilderCustomizer configuration");
+            jsonMapperBuilder.disable(DateTimeFeature.WRITE_DATES_AS_TIMESTAMPS);
+            jsonMapperBuilder.enable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        };
+    }
+
+    @Bean
+    public XmlMapperBuilderCustomizer xmlMapperBuilderCustomizer() {
+        return xmlMapperBuilder -> {
+            log.info("Applying XmlMapperBuilderCustomizer configuration");
+            xmlMapperBuilder.disable(DateTimeFeature.WRITE_DATES_AS_TIMESTAMPS);
+            xmlMapperBuilder.enable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        };
+    }
+}
